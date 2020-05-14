d4507e2576793278cd1dbbac218bbe2f7cba26a2
Nauha Elisa <elisa.nauha@kela.fi>
2020-05-14 13:39:11 +0300

    Do puhelinluettelo backend step4 delete

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-const persons = [
+let persons = [
     {
         "name": "Arto Hellas",
         "number": "040-123456",
@@ -45,7 +45,12 @@ app.get('/api/persons/:id', (req, res) => {
     else {
         res.status(404).end()
     }
+})
 
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(person => person.id !== id)
+    res.status(204).end()
 })
 
 app.get('/api/persons', (req, res) => {
