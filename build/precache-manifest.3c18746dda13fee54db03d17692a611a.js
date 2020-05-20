4593cbd3a5551fc22bf5bae4f782489ced3c85fb
elisanauha <elisa.nauha@gmail.com>
2020-05-20 14:34:39 +0300

    3.11 puhelinluettelo full stack Add frontend build

---
+++ build/precache-manifest.3c18746dda13fee54db03d17692a611a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3936d35f7e1dbc675c56365a6149d5c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e9fd68c3a44decddc5b",
+    "url": "/static/css/main.b1c374e2.chunk.css"
+  },
+  {
+    "revision": "f5f7b13d34d14b56302e",
+    "url": "/static/js/2.076799b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.076799b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e9fd68c3a44decddc5b",
+    "url": "/static/js/main.b619e590.chunk.js"
+  },
+  {
+    "revision": "d6d4691db26ca793a81a",
+    "url": "/static/js/runtime-main.4f3ee9ca.js"
+  }
+]);
