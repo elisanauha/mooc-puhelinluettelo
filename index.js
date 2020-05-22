83e0176f30eb7307a4e5fb6e51a13d1320fdd8ff
elisanauha <elisa.nauha@gmail.com>
2020-05-22 13:29:07 +0300

    Do 3.16-17 puehlinluettolo ja tietokanta step4-5

--- index.js
+++ index.js
@@ -66,16 +66,41 @@ app.get("/api/persons/:id", (req, res) => {
   }
 });
 
-app.delete("/api/persons/:id", (req, res) => {
-  Person.findByIdAndRemove(req.param.id)
+app.delete("/api/persons/:id", (req, res, next) => {
+  Person.findByIdAndRemove(req.params.id)
     .then((result) => {
       res.status(204).end();
     })
     .catch((error) => {
-      console.log(error);
-      //next(error))
+      next(error);
     });
 });
+
+app.put("/api/persons/:id", (req, res, next) => {
+  const body = req.body;
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      res.json(updatedPerson);
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message);
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformed id" });
+  }
+  next(error);
+};
+
+app.use(errorHandler);
 
 app.get("/api/persons", (req, res) => {
   Person.find({}).then((foundPeople) => {
